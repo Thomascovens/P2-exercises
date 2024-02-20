7158ed72fd80fcbff9321d9736cadc95e01cf7e3
Thomas Covens <thomas.covens@student.ucll.be>
2024-02-20 09:25:56 +0100

    Intro

--- 01-basic-python/04-lists/05-median/student.py
+++ 01-basic-python/04-lists/05-median/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+
+def median(ns):
+    sort = sorted(ns)
+    i = len(sort) //2
+    if len(sort)%2==0:
+        return (sort[i - 1] + sort[i]) /2
+    
+    else:
+        return sort[i]
+    
+median([50, 20, 100, 40, 20])
